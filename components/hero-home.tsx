c5e39c06ab03e32a47fe4a32f9c9bc535aa8c526
jose <jmvilla2301>
2025-04-19 02:22:00 -0500

    Creacion de modulo de reuniones - 2025/04/19

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,6 +1,8 @@
 import VideoThumb from "@/public/images/hero-image-01.jpg";
 import ModalVideo from "@/components/modal-video";
 
+import Link from "next/link";
+
 export default function HeroHome() {
   return (
     <section>
@@ -26,17 +28,18 @@ export default function HeroHome() {
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
-                  <a
+                  <Link
                     className="btn group mb-4 w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="#0"
+                    href="/meeting"
                   >
                     <span className="relative inline-flex items-center">
-                      Start Building
+                      Ir a Reuniones
+
                       <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
                         -&gt;
                       </span>
                     </span>
-                  </a>
+                  </Link>
                 </div>
                 <div data-aos="fade-up" data-aos-delay={600}>
                   <a
